a9a4c41b03bc0ea67a462bbb12c1562adfde076b
Raíssa Vilela <30978411+raissavilela@users.noreply.github.com>
2020-08-19 19:20:46 -0300

    Create Server.java

---
+++ application/Server.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+class Server implements Runnable { 
+
+  private int port; 
+  private InetAddress group; 
+  private MulticastSocket socket; 
+  private static final int MAX_LEN = 1000; 
+
+  public Server(MulticastSocket socket,InetAddress group,int port){ 
+      this.socket = socket; 
+      this.group = group; 
+      this.port = port; 
+  } 
+      
+  @Override
+  public void run(){ 
+    while(!Client.finished){ 
+      byte[] buffer = new byte[Server.MAX_LEN]; 
+      DatagramPacket datagram = new
+      DatagramPacket(buffer,buffer.length,group,port); 
+      String message; 
+      try { 
+        socket.receive(datagram); 
+        message = new String(buffer,0,datagram.getLength(),"UTF-8"); 
+        if(!message.startsWith(Client.username)){
+          System.out.println(message); 
+        }
+      } catch(IOException e){ 
+         System.out.println(""+Client.username+" está sendo desconectado..."); 
+      } 
+    } 
+  } 
+}
